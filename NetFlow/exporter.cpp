a4913f549fc532a925053abca9651d9dee35192b
Sang-geon Yun <c0510gy@protonmail.ch>
2020-05-05 20:04:05 +0900

    add: flowHandler on exporter.cpp

--- NetFlow/exporter.cpp
+++ NetFlow/exporter.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
+#include <queue>
+#include <thread>
 #include "UDP-Client.h"
 #include "PacketCapture.h"
 #include "Types.h"
 using namespace std;
 
+queue<Flow> q;
+
 void callback(Flow flow){
-    
+    q.push(flow);
 }
-
+void flowHandler(){
+    while(true){
+        if(q.empty())
+            continue;
+        Flow flow = q.front(); q.pop();
+        cout << flow.protocol << " " << flow.srcIP << ":" << flow.srcPort << " " << flow.desIP << ":" << flow.desPort << " " << flow.length << endl;
+    }
+}
 int main(){
-    initPCAP();
+    thread thread_obj(flowHandler);
 
+    initPCAP();
 
     return 0;
 }
